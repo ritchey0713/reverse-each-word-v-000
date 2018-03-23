31481d8e8275aa40536d5066d8c58aa979f0200a
David Ritchey <ritchey0713@gmail.com>
2018-03-23 20:10:57 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -12,8 +12,8 @@ def reverse_each_word(sentence1)
   arr=sentence1.split(" ")
   
   arr.collect do |mirror|
-    new_arr << mirror.reverse
-    new_arr.join(" ")
+    mirror.reverse
+    mirror.join(" ")
   end
 end
     
