0633e7b76a47fb73bc81f2daa57c2d4529da33cc
David Ritchey <ritchey0713@gmail.com>
2018-03-23 20:08:27 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
-def reverse_each_word(sentence1)
-arr = sentence1.split(" ")
-new_arr=[]
-arr.each do |mirror| 
-  new_arr << mirror.reverse
-end
-return new_arr.join(" ")
+#def reverse_each_word(sentence1)
+#arr = sentence1.split(" ")
+#new_arr=[]
+#arr.each do |mirror| 
+#  new_arr << mirror.reverse
+#end
+#return new_arr.join(" ")
 
-end
+#end
